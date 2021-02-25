dfa1405d71a2be52b54bc1719430b2346b93401c
jharry444 <jordan.harrison@qa.com>
2021-02-25 11:02:54 +0000

    init commit

---
+++ src/test/java/com/qa/penguins/SpringPenguinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qa.penguins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPenguinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
