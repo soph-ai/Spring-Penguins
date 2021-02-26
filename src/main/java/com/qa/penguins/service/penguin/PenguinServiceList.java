97290d3edf1300b970bd30fb0ea6c02488e74214
jharry444 <jordan.harrison@qa.com>
2021-02-26 11:35:51 +0000

    PenguinServiceList now correctly implements the PenguinService interface

--- src/main/java/com/qa/penguins/service/penguin/PenguinServiceList.java
+++ src/main/java/com/qa/penguins/service/penguin/PenguinServiceList.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Service;
 import com.qa.penguins.domain.Penguin;
 
 @Service
-public class PenguinServiceList {
+public class PenguinServiceList implements PenguinService {
 
 	private List<Penguin> penguins;
 
@@ -16,20 +16,24 @@ public class PenguinServiceList {
 		this.penguins = penguins;
 	}
 
+	@Override
 	public Penguin createPenguin(Penguin penguin) {
 		this.penguins.add(penguin);
 		Penguin added = this.penguins.get(this.penguins.size() - 1);
 		return added;
 	}
 
+	@Override
 	public List<Penguin> getPenguin() {
 		return this.penguins;
 	}
 
+	@Override
 	public Penguin getPenguinById(int id) {
 		return this.penguins.get(id);
 	}
 
+	@Override
 	public Penguin removePenguin(int id) {
 		return this.penguins.remove(id);
 	}
